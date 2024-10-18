2960eb4a42bc337d681d5e5db9f316c6ca6e9bf0
Maxmunene git config --global user.name <munene.mbaabu@strathmore.edu>
2024-10-18 12:07:22 +0300

    sanctum

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -13,7 +13,8 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        //
+        $categories = category::all();
+        return $categories;
     }
 
     /**
